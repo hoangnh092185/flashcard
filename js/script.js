ac1eb6507481f01ce96de6d6b4a87d52341b29e1
Adam Gorbahn and Nhat Hoang <nhat@>
2016-08-09 14:35:21 -0700

    Creates jquery styles for each of definition.

---
+++ js/script.js
@@ -0,0 +1,79 @@
+$(document).ready(function () {
+
+
+  $("#js").click(function () {
+    $(".js").toggle();
+  });
+
+  $("#operators").click(function () {
+    $(".operatorsText").slideToggle();
+  });
+
+  $("#variables").click(function () {
+    $(".variablesText").fadeToggle();
+  });
+
+  $("#vnc").click(function () {
+    $(".vncText").slideToggle();
+  });
+
+  $("#functions").click(function () {
+    $(".functionsText").slideToggle();
+  });
+
+  $("#methods").click(function () {
+    $(".methodsText").slideToggle();
+  });
+
+  $("#arguments").click(function () {
+    $(".argumentsText").slideToggle();
+  });
+
+  $("#parameters").click(function () {
+    $(".parametersText").slideToggle();
+  });
+
+  $("#return").click(function () {
+    $(".returnText").slideToggle();
+  });
+
+  $("#cm").click(function () {
+    $(".cmText").slideToggle();
+  });
+
+  $("#strings").click(function () {
+    $(".stringsText").slideToggle();
+  });
+
+  $("#booleans").click(function () {
+    $(".boolText").slideToggle();
+  });
+
+  $("#undefined").click(function () {
+    $(".undefText").slideToggle();
+  });
+
+  $("#nan").click(function () {
+    $(".nanText").slideToggle();
+  });
+
+  $("#escape").click(function () {
+    $(".escapeText").slideToggle();
+  });
+  
+  $("#alert").click(function () {
+    $(".alertText").slideToggle();
+  });
+
+  $("#comments").click(function () {
+    $(".commentsText").slideToggle();
+  });
+
+  $("#jquery").click(function () {
+    $(".jqueryText").slideToggle();
+  });
+
+  $("#attributes").click(function () {
+    $(".attributesText").slideToggle();
+  });
+});
